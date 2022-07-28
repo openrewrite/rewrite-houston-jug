8f0dd46e572337198e9ab6f2dfceaa77a49a4edd
Jonathan Schneider <jkschneider@gmail.com>
2022-07-27 21:58:28 -0400

    UnwrapRepeatableAnnotations

--- src/main/java/org/openrewrite/houston/UnwrapRepeatableAnnotations.java
+++ src/main/java/org/openrewrite/houston/UnwrapRepeatableAnnotations.java
@@ -4,7 +4,6 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.ListUtils;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.tree.J;
@@ -36,6 +35,9 @@ public class UnwrapRepeatableAnnotations extends Recipe {
             public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
                 J.MethodDeclaration m = super.visitMethodDeclaration(method, ctx);
                 List<J.Annotation> ann = unwrap(m.getLeadingAnnotations());
+                if (ann.isEmpty()) {
+                    return m;
+                }
                 return maybeAutoFormat(m, m.withLeadingAnnotations(ann), ann.get(ann.size() - 1), ctx,
                         getCursor().getParentOrThrow());
             }
@@ -43,7 +45,23 @@ public class UnwrapRepeatableAnnotations extends Recipe {
             @Override
             public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
                 J.ClassDeclaration c = super.visitClassDeclaration(classDecl, ctx);
-                return c.withLeadingAnnotations(unwrap(c.getLeadingAnnotations()));
+                List<J.Annotation> ann = unwrap(c.getLeadingAnnotations());
+                if (ann.isEmpty()) {
+                    return c;
+                }
+                return maybeAutoFormat(c, c.withLeadingAnnotations(ann), ann.get(ann.size() - 1), ctx,
+                        getCursor().getParentOrThrow());
+            }
+
+            @Override
+            public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
+                J.VariableDeclarations v = super.visitVariableDeclarations(multiVariable, ctx);
+                List<J.Annotation> ann = unwrap(v.getLeadingAnnotations());
+                if (ann.isEmpty()) {
+                    return v;
+                }
+                return maybeAutoFormat(v, v.withLeadingAnnotations(ann), ann.get(ann.size() - 1), ctx,
+                        getCursor().getParentOrThrow());
             }
 
             private List<J.Annotation> unwrap(List<J.Annotation> annotations) {
